a6700efae9f9fd26b3b4ee25c66a32d1d25784a2
Gavin Nicol <ngavin@umich.edu>
2016-02-28 19:32:24 -0500

    first loop working

--- initDB.py
+++ initDB.py
@@ -1,33 +1,38 @@
 #!/usr/bin/python
 
+import json
 import psycopg2
 import sys
 
-con = None
-
-try:
-    con = psycopg2.connect(database="civisAnalytics", user="gavin", host="/tmp/")
-    cur = con.cursor()
-
-    cur.execute(
-        "PREPARE loadData as "
-        "INSERT INTO Stop VALUES"
-        "($1, $2, $3, $4, $5, $6, $7, $8, $9)"
-        )
-
-    cur.execute(
-        "EXECUTE loadData ('{id}', '{on_street}', '{cross_street}', '{routes}', '{boardings}', '{alightings}', '{creation}', '{daytype}', POINT'{coordinates}')".format(
-        id=1, on_street="JOY", cross_street="HAGGERTY", routes="{1,3}", boardings=28.33, alightings=1.0, creation="2010-2-10", daytype="Weekday", coordinates="(20.200, -32.003)"
-        ))
-
-    con.commit()
-
-except psycopg2.DatabaseError, e:
-    print 'Database error:\n %s' % e
-    sys.exit(1)
-
-finally:
-    if con:
-        con.close()
-    sys.exit(0)
+with open("sql/data.json") as data_file:
     
+    con = None
+    try:
+        con = psycopg2.connect(database="civisAnalytics", user="gavin", host="/tmp/")
+        cur = con.cursor()
+
+        cur.execute(
+            "PREPARE loadData as "
+            "INSERT INTO Stop VALUES"
+            "($1, $2, $3, $4, $5, $6, $7, $8, $9)"
+            )
+
+        data = json.load(data_file)
+        for stop in data["data"]:
+            tmp = "EXECUTE loadData ('{id}', '{on_street}', '{cross_street}', ARRAY[{routes}], '{boardings}', '{alightings}', '{creation}', '{daytype}', POINT'({x},{y})')".format(id=stop[8], on_street=stop[9], cross_street=stop[10], routes=stop[11], boardings=stop[12], alightings=stop[13], creation=stop[14], daytype=stop[15], x=stop[16][1], y=stop[16][2])
+            print tmp;
+            cur.execute(
+                tmp)
+            con.commit()
+            break
+
+        con.commit()
+
+    except psycopg2.DatabaseError, e:
+        print 'Database error:\n %s' % e
+        sys.exit(1)
+
+    finally:
+        if con:
+            con.close()
+        sys.exit(0)
