491894776f648ba63a8ab42f1bd278f78823d6f2
Gavin Nicol <ngavin@umich.edu>
2016-02-29 18:59:21 -0500

    google maps displayed on page

--- app/app.py
+++ app/app.py
@@ -6,6 +6,8 @@ app.config.from_object("config")
 index = Blueprint('index', __name__, template_folder='templates')
 app.register_blueprint(index)
 
+api_key = "AIzaSyDAEWcpvx6no3lvWU26WiY4KmH3ZKGLXTc"
+
 @app.route('/')
 def hello():
     return render_template("index.html")
