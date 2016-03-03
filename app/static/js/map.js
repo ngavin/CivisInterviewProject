bb42c37478536bd8923b689ee3af50cc16903d3d
Gavin Nicol <ngavin@umich.edu>
2016-03-03 15:54:43 -0500

    custom event

--- app/static/js/map.js
+++ app/static/js/map.js
@@ -1,26 +1,27 @@
 function initMap() {
-    console.log("initing map");
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 41.854, lng: -87.709},
         zoom: 10
     });
     console.log("map init complete");
-    drawMarkers();
+    var mapReady = new CustomEvent("mapReady");
+    window.dispatchEvent(mapReady);
 }   
 
 function drawMarkers() {
+    console.log("drawing markers");
     var xhttp = new XMLHttpRequest();
     xhttp.open("GET", "/data/markers", true)
     xhttp.send();
     console.log("sent request");
     xhttp.onreadystatechange = function() {
-        console.log("readyState change");
-
-        if (xhttp.readyState == 4 && xhttp.status == 200) {
-            console.log(xhttp.response)
-        }
-        else {
-            console.error("erorr " + xhttp.status + "occurred at state " + xhttp.readyState);
+        if (xhttp.readyState == 4) {
+            if (xhttp.status == 200) {
+                console.log("recieved data");
+            }
+            else {
+                console.error("erorr " + xhttp.status);
+            }
         }
     }
 }
