12bcaff363ac35cad29114577583c431e091f4f7
Gavin Nicol <ngavin@umich.edu>
2016-03-03 17:32:03 -0500

    working on InfoWindows

--- app/static/js/map.js
+++ app/static/js/map.js
@@ -3,10 +3,12 @@ function initMap() {
         center: {lat: 41.854, lng: -87.709},
         zoom: 10
     });
+    // map.infoWindow = new google.maps.InfoWindow();
+    window.addEventListener("mapReady", createMarkers);
     window.dispatchEvent(new CustomEvent("mapReady"));
 }   
 
-function drawMarkers() {
+function createMarkers() {
     var xhttp = new XMLHttpRequest();
     xhttp.open("GET", "/data/markers", true)
     xhttp.send();
@@ -14,7 +16,7 @@ function drawMarkers() {
         if (xhttp.readyState == 4) {
             if (xhttp.status == 200) {
                 var stops = JSON.parse(xhttp.responseText);
-                stops.forEach(drawMarker);
+                stops.forEach(createMarker);
             }
             else {
                 console.error("erorr " + xhttp.status);
@@ -23,10 +25,17 @@ function drawMarkers() {
     }
 }
 
-function drawMarker(stop) {
-    new google.maps.Marker({
+function createMarker(stop) {
+    var marker = new google.maps.Marker({
         position: {lat: parseFloat(stop[1]), lng: parseFloat(stop[2])},
         title: stop[0],
         map: map
     });
+    marker.addListener("click", displayInfoWindow, marker);
+}
+
+// Need to find a way to access the Marjer object
+function displayInfoWindow(e, marker) {
+    console.log(marker);
+    // console.log(marker.getPosition());
 }
